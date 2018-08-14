chrisallenlane/novahot

commit 3c847077b47c19f001f973d9c2899bc35803e0c3

adapter-edit: syntax-highlighting
Modified `adapter-edit` to save temporary files with a file extension
that matches the source files. This allows editors to apply the
appropriate syntax highlighting automatically.

app/cmd/shell/adapter-edit.js
@@ -12,7 +12,11 @@ module.exports = function(config, req, cmd, callback) {
   // tokenize the input string
   const tokenized = shellwords.split(cmd);
   const src       = tokenized[1];
-  const dst       = tmp.fileSync().name; 
+
+  // Generate a temporary file with an extension that mathces the source file.
+  // The matching extension is important to allow editors to choose the
+  // appropriate syntax-highlighting automatically.
+  const dst = tmp.fileSync({ postfix: path.extname(src) }).name;
 
   // assert that src was provided
   if (! src) {

commit 8962e4c639b9bda5bbd40c52df250cc90bdab29f

Bug fix
Fixed a bug that was introduced in 3c847077b47c19f, whereby a value of
`undefined` could potentially be passed to `path.extname`, causing a
crash.

See c561c3fa052f56 for related information.

app/cmd/shell/adapter-edit.js
@@ -11,7 +11,7 @@ module.exports = function(config, req, cmd, callback) {
 
   // tokenize the input string
   const tokenized = shellwords.split(cmd);
-  const src       = tokenized[1];
+  const src       = tokenized[1] || '';
 
   // Generate a temporary file with an extension that mathces the source file.
   // The matching extension is important to allow editors to choose the